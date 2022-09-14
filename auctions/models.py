153fdf281377a0b13670e4ccc5575afb3cd347c4
David <djmiller1008@yahoo.com>
2022-09-14 10:17:17 +0800

    Initial Commit

---
+++ auctions/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=300)
+    starting_bid = models.PositiveIntegerField()
+    image = models.ImageField(blank=True)
+    category = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+
+    def __str__(self):
+        return f"{self.title}: {self.description}"
+
+
+    
