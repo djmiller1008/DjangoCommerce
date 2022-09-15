86f89eb0a6d001671f43cc6550d1b0d986497b62
David <djmiller1008@yahoo.com>
2022-09-15 11:04:21 +0800

    Make image field in model a charfield just for the image url

--- auctions/models.py
+++ auctions/models.py
@@ -8,9 +8,10 @@ class Listing(models.Model):
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=300)
     starting_bid = models.PositiveIntegerField()
-    image = models.ImageField(blank=True)
+    image = models.CharField(max_length=200)
     category = models.CharField(max_length=20)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.title}: {self.description}"
