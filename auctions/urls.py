ebd70fa00abc5f5b37007eadb12b6f33f41cdcda
David <djmiller1008@yahoo.com>
2022-09-16 23:10:35 +0800

    Add urls for category and categories routes

--- auctions/urls.py
+++ auctions/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "commerce"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("new", views.new, name="new")
+    path("new", views.new, name="new"),
+    path("categories", views.categories, name="categories"),
+    path("categories/<str:category>", views.category, name="category")
 ]
