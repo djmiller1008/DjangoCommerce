1b2e893a1956ee75598fa1fe67a1fdabdff3cdd8
David <djmiller1008@yahoo.com>
2022-09-20 14:48:59 +0800

    Add comment and close listing routes

--- auctions/urls.py
+++ auctions/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path("categories/<str:category>", views.category, name="category"),
     path("listing/<int:id>", views.listing, name="listing"),
     path("watchlist/<int:listing_id>", views.toggle_watchlist, name="toggle_watchlist"),
-    path("watchlist", views.watchlist, name="watchlist")
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("comment/<int:listing_id>", views.add_comment, name="add_comment"),
+    path("close_auction/<int:listing_id>", views.close_listing, name="close_listing")
 ]
