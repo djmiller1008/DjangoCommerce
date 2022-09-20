b2fe0c6ce48965f19a84458ed5e5c985f4a2c47e
David <djmiller1008@yahoo.com>
2022-09-20 14:49:22 +0800

    auctions/models.py

--- auctions/models.py
+++ auctions/models.py
@@ -21,6 +21,7 @@ class Listing(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="listings", null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     created_at = models.DateTimeField(auto_now_add=True)
+    closed = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.title}: {self.description}"
@@ -34,3 +35,9 @@ class Bid(models.Model):
 class Watchlist(models.Model):
     listing=models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="watchlist")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist")
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
